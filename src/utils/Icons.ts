37cddf7b682bbb2f2259497b96e85d331f2f7f9e
DevYuns <ydhun93@gmail.com>
2021-05-10 14:10:40 +0900

    Set to load Assets like 'png' & 'svg'

---
+++ src/utils/Icons.ts
@@ -0,0 +1,9 @@
+import icAdd from '../../assets/icons/add.png';
+import icTrash from '../../assets/icons/trash.png';
+import svgCheck from '../../assets/icons/svgCheck.svg';
+
+export const IC_ADD = icAdd;
+export const IC_TRASH = icTrash;
+export const SvgCheck = svgCheck;
+
+export default [IC_ADD, IC_TRASH];

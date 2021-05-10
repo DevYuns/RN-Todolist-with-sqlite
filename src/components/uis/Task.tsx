17285a5c371cbd3f19be2acf5f5af977f35535fa
DevYuns <ydhun93@gmail.com>
2021-05-10 13:45:13 +0900

    Constructing View hierarchy & Make a basic component

---
+++ src/components/uis/Task.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Task: React.FC = () => {
+  return (
+    <View>
+      <Text>Task 1</Text>
+    </View>
+  );
+};
+
+export default Task;
